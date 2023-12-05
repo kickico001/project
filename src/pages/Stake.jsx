2ff5568d68b4ea679d26954e580563f821cc302f
lazynerd <84862778+lazynerd-studios@users.noreply.github.com>
2023-12-05 19:29:22 +0100

    link

--- src/pages/Stake.jsx
+++ src/pages/Stake.jsx
@@ -1,5 +1,6 @@
 import { toast } from "react-toastify";
 import logo from "../assets/Cardano.png";
+import { useLink } from 'react-router-dom';
 import axios from "axios";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from '../initFirebase';
@@ -98,9 +99,9 @@ const Stake = () => {
           team. 0% commission +🔥APY %
         </h2>
         <div className="h-full bg-black bg-opacity-50 flex flex-col items-center space-y-8 px-6 py-8 rounded-lg text-white backdrop-filter backdrop-blur-md">
-          <button className="w-full bg-gray-700 text-gray-400 px-2 py-2 rounded-full font-semibold">
+          <Link to={useLink('/Home')} className="w-full bg-gray-700 text-gray-400 px-2 py-2 rounded-full font-semibold">
             Stake ADA
-          </button>
+          </Link>
         </div>
       </div>
     </div>
